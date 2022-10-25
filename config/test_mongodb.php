f7bfc1e60db8d01650480d254d0b28541bf80ffc
Dzil <ahmadfadlydziljalal@gmail.com>
2022-10-25 18:01:29 +0700

    Remove test_mongodb

--- config/test_mongodb.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mongo = require __DIR__ . '/mongodb.php';
-// test database! Important not to run tests on production or development databases
-$mongo['defaultDatabaseName'] = getenv('MONGO_INITDB_DATABASE_TEST');
-
-return $mongo;
