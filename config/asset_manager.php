47ab01eca1788776123ebbcad0ab768b483c6e22
Dzil <ahmadfadlydziljalal@gmail.com>
2022-12-01 13:38:26 +0700

    Remove search bar di navbar

--- config/asset_manager.php
+++ config/asset_manager.php
@@ -3,19 +3,9 @@
 return [
     'dirMode' => 0755,
     'bundles' => [
-        'yii\web\JqueryAsset' => [
-            'js' => [
-                YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
-            ]
-        ],
         'yii\bootstrap5\BootstrapAsset' => [
             'css' => [],
         ],
-        // 'yii\bootstrap5\BootstrapPluginAsset' => [
-        //     'js' => [
-        //         YII_ENV_DEV ? 'js/bootstrap.bundle.js' : 'js/bootstrap.bundle.min.js',
-        //     ]
-        // ]
     ],
     'appendTimestamp' => true,
     'forceCopy' => YII_ENV_DEV,
