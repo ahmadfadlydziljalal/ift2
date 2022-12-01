9aef8af1023f89dac297955b5011e307bc53113e
Dzil <ahmadfadlydziljalal@gmail.com>
2022-12-01 13:21:17 +0700

    Bug bootstrap bundle min js 404

--- config/asset_manager.php
+++ config/asset_manager.php
@@ -11,11 +11,11 @@ return [
         'yii\bootstrap5\BootstrapAsset' => [
             'css' => [],
         ],
-        'yii\bootstrap5\BootstrapPluginAsset' => [
-            'js' => [
-                YII_ENV_DEV ? 'js/bootstrap.bundle.js' : 'js/bootstrap.bundle.min.js',
-            ]
-        ]
+        // 'yii\bootstrap5\BootstrapPluginAsset' => [
+        //     'js' => [
+        //         YII_ENV_DEV ? 'js/bootstrap.bundle.js' : 'js/bootstrap.bundle.min.js',
+        //     ]
+        // ]
     ],
     'appendTimestamp' => true,
     'forceCopy' => YII_ENV_DEV,
