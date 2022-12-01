85f28afb9dc3196fb105d16513781252368c0733
Dzil <ahmadfadlydziljalal@gmail.com>
2022-12-01 14:32:21 +0700

    Try jquery cdn

--- config/asset_manager.php
+++ config/asset_manager.php
@@ -1,12 +1,21 @@
 <?php
 
+use yii\bootstrap5\BootstrapAsset;
+use yii\web\JqueryAsset;
+
 return [
-    'dirMode' => 0755,
-    'bundles' => [
-        'yii\bootstrap5\BootstrapAsset' => [
-            'css' => [],
-        ],
-    ],
-    'appendTimestamp' => true,
-    'forceCopy' => YII_ENV_DEV,
+   'dirMode' => 0755,
+   'bundles' => [
+      JqueryAsset::class => [
+         'js' => [
+            //YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js',
+            YII_ENV_DEV ? 'https://code.jquery.com/jquery-3.5.1.js' : 'https://code.jquery.com/jquery-3.5.1.min.js'
+         ]
+      ],
+      BootstrapAsset::class => [
+         'css' => [],
+      ],
+   ],
+   'appendTimestamp' => true,
+   'forceCopy' => YII_ENV_DEV,
 ];
