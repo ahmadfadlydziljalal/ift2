9a63eb65cc0794faba1aa296b1690925a08c90a2
Dzil <ahmadfadlydziljalal@gmail.com>
2022-12-08 14:36:30 +0700

    Fixed view quotation bug pph label

--- views/quotation/_item.php
+++ views/quotation/_item.php
@@ -22,7 +22,7 @@ use yii\web\View;
         </div>
 
         <div class="row ">
-            <div class="col-12 col-sm-12 col-md-4 col-lg-2 mb-3 mb-sm-3 mb-md-3 mb-lg-1">
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 mb-3 mb-sm-3 mb-md-3 mb-lg-1">
                 <div class="card bg-transparent border  h-100 w-100">
                     <div class="card-body">
                         <small class="text-muted">
@@ -33,13 +33,13 @@ use yii\web\View;
                     <div class="card-footer p-3">
 
                        <?= Html::a('<i class="bi bi-eye"></i>', ['quotation/view', 'id' => $model->id], [
-                          'class' => 'text-primary'
+                          'class' => 'btn btn-primary'
                        ]) ?>
                        <?= Html::a('<i class="bi bi-pen-fill"></i>', ['quotation/update', 'id' => $model->id], [
-                          'class' => 'text-primary'
+                          'class' => 'btn btn-primary'
                        ]) ?>
                        <?= Html::a('<i class="bi bi-trash"></i>', ['quotation/delete', 'id' => $model->id], [
-                          'class' => 'text-danger',
+                          'class' => 'btn btn-danger',
                           'data' => [
                              'confirm' => 'Apakah anda yakin menghapus service quotation ini ?',
                              'method' => 'post'
@@ -58,9 +58,7 @@ use yii\web\View;
                             </div>
                             <div>
                                 <strong><?= $model->mataUang->singkatan ?> <?= Yii::$app->formatter->asDecimal($model->quotationBarangsTotal, 2) ?></strong>
-
                             </div>
-
                         </div>
                     </div>
 
@@ -127,42 +125,43 @@ use yii\web\View;
                 </div>
 
             </div>
-            <div class="col-12 col-sm-12 col-md-4 col-lg-2 mb-3 mb-sm-3 mb-md-3 mb-lg-1">
-                <div class="card bg-transparent border  h-100 w-100">
-                    <div class="card-body">
-                        <div class="d-flex flex-column gap-1">
-                            <div>
-                                <p class="text-muted text-nowrap">Term & Condition</p>
-                            </div>
-                            <div>
-                                <strong><?= count($model->quotationTermAndConditions) ?></strong>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card-footer p-3">
-                       <?php if (empty($model->quotationTermAndConditions)) : ?>
-
-                          <?= Html::a('<i class="bi bi-plus-lg"></i>', ['quotation/create-term-and-condition', 'id' => $model->id], ['class' => 'text-primary']) ?>
-
-                       <?php else : ?>
-
-                          <?php /* @see \app\controllers\QuotationController::actionUpdateServiceQuotation() */ ?>
-                          <?= Html::a('<i class="bi bi-pen-fill"></i>', ['quotation/update-term-and-condition', 'id' => $model->id], ['class' => 'text-primary']) ?>
-
-                          <?php /* @see \app\controllers\QuotationController::actionDeleteServiceQuotation() */ ?>
-                          <?= Html::a('<i class="bi bi-trash-fill"></i>', ['quotation/delete-term-and-condition', 'id' => $model->id], [
-                             'class' => 'text-danger',
-                             'data' => [
-                                'confirm' => 'Apakah anda yakin menghapus term and condition quotation ini ?',
-                                'method' => 'post'
-                             ]
-                          ]) ?>
-
-                       <?php endif; ?>
-                    </div>
-                </div>
-            </div>
+            <!--            <div class="col-12 col-sm-12 col-md-4 col-lg-2 mb-3 mb-sm-3 mb-md-3 mb-lg-1">-->
+            <!--                <div class="card bg-transparent border  h-100 w-100">-->
+            <!--                    <div class="card-body">-->
+            <!--                        <div class="d-flex flex-column gap-1">-->
+            <!--                            <div>-->
+            <!--                                <p class="text-muted text-nowrap">Term & Condition</p>-->
+            <!--                            </div>-->
+            <!--                            <div>-->
+            <!--                                <strong>-->
+           <?php //echo  count($model->quotationTermAndConditions) ?><!--</strong>-->
+            <!--                            </div>-->
+            <!--                        </div>-->
+            <!--                    </div>-->
+            <!---->
+            <!--                    <div class="card-footer p-3">-->
+            <!--                       --><?php //if (empty($model->quotationTermAndConditions)) : ?>
+            <!---->
+            <!--                          --><?php //echo  Html::a('<i class="bi bi-plus-lg"></i>', ['quotation/create-term-and-condition', 'id' => $model->id], ['class' => 'text-primary']) ?>
+            <!---->
+            <!--                       --><?php //else : ?>
+            <!---->
+            <!--                          --><?php ///* @see \app\controllers\QuotationController::actionUpdateServiceQuotation() */ ?>
+            <!--                          --><?php //echo  Html::a('<i class="bi bi-pen-fill"></i>', ['quotation/update-term-and-condition', 'id' => $model->id], ['class' => 'text-primary']) ?>
+            <!---->
+            <!--                          --><?php ///* @see \app\controllers\QuotationController::actionDeleteServiceQuotation() */ ?>
+            <!--                          --><?php //echo  Html::a('<i class="bi bi-trash-fill"></i>', ['quotation/delete-term-and-condition', 'id' => $model->id], [
+           //                             'class' => 'text-danger',
+           //                             'data' => [
+           //                                'confirm' => 'Apakah anda yakin menghapus term and condition quotation ini ?',
+           //                                'method' => 'post'
+           //                             ]
+           //                          ]) ?>
+            <!---->
+            <!--                       --><?php //endif; ?>
+            <!--                    </div>-->
+            <!--                </div>-->
+            <!--            </div>-->
             <div class="col-12 col-sm-12 col-md-4 col-lg-2 mb-3 mb-sm-3 mb-md-3 mb-lg-1">
                 <div class="card bg-transparent border  h-100 w-100">
                     <div class="card-body">
