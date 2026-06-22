5980f8a84203dc976fbe368698ac298490209e6b
afdj <ahmadfadlydziljalal@gmail.com>
2026-06-22 12:39:22 +0700

    feature: tambah barcode untuk pdf nya

--- components/QrCodeStockGenerator.php
+++ components/QrCodeStockGenerator.php
@@ -9,15 +9,22 @@ class QrCodeStockGenerator extends Component {
 
     public string $text;
     public ?string $filename = null;
-    public int $size = 125;
-    public float $margin = 5;
+
+    public int $size = 100;
+    public int $margin = 5;
 
     private function generate(): QrCode {
         return (new QrCode($this->text))
             ->setSize($this->size)
-            ->setMargin($this->margin);
+            ->setMargin((int)$this->margin)
+//            ->setErrorCorrectionLevel('H')
+//            ->setLogo(Yii::getAlias('@webroot/images/logo-qr.png'))
+            ;
     }
 
+    /**
+     * @return string
+     */
     public function toFile(): string {
         $qrCode = $this->generate();
         $path = sys_get_temp_dir() . '/' . $this->filename;
@@ -25,6 +32,9 @@ class QrCodeStockGenerator extends Component {
         return $path;
     }
 
+    /**
+     * @return string
+     */
     public function toWriteDataUri(): string {
         $qrCode = $this->generate();
         return $qrCode->writeDataUri();
--- views/stock/preview_print_sticker.php
+++ views/stock/preview_print_sticker.php
@@ -10,14 +10,26 @@ use yii\web\View;
 /* @var $path bool|int */
 ?>
 
-
-<div style="width: 100%; height: 100%;">
-    <div style="float: left; width: 40%">
-        <?= Html::img($path) ?>
+<div style="width: 100%;">
+    <div style="float: left; text-align: left; width: 20%; margin-bottom: 1em; ">
+        <?= Html::img(Yii::getAlias('@webroot/images/logo.png'), ['width' => '.5cm']) ?><br/>
+        <span style="font-size: .25em">Indo Formosa Trading, PT</span>
     </div>
-    <div style="float: right; text-align: right; font-size: .90em; width: 59%">
-        <p><?= $model->part_number ?><br/><?= $model->ift_number ?></p>
+    <div style="float: right; text-align: right; width: 80%; margin-bottom: 1em">
+        <barcode code="<?= $model->id ?>" type="C128B" height="0.5" text="1"></barcode>
     </div>
     <div style="clear: both"></div>
-    <p style="font-size: .75em; margin-bottom: 0;margin-top: 4pt"><?= StringHelper::truncate($model->nama, 32) ?></p>
 </div>
+
+<div style="width: 100%;">
+    <div style=" float: left; width: 30%">
+        <?= Html::img($path) ?>
+    </div>
+    <div style="float: right; text-align: right;  width: 69%">
+        <span style="font-size: .5em;"><?= $model->part_number ?> | <?= $model->ift_number ?> | <?= $model->id ?> </span><br/>
+        <span style="font-size: .5em;"><?= StringHelper::truncate($model->nama, 48) ?></span><br/>
+        <small style="font-size: .25em;"><?= Yii::$app->formatter->asDatetime(date('Y-m-d H:i:s')) ?></small>
+    </div>
+    <div style="clear: both"></div>
+</div>
+
